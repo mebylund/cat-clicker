4596b231cb5ad954b32014bc0f6a4977d06f65d2
mebylund <35643665+mebylund@users.noreply.github.com>
2018-10-04 18:50:29 -0700

    Added Aria

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 var clicks = 0;
-
+var cat1 = 'Clover';
+var cat2 = 'Aria';
 function displayClicks() {
-    document.getElementById('clck').innerHTML = 'You clicked the cat ' + clicks + ' times!';
+    document.getElementById('clck').innerHTML = 'You clicked the cats ' + clicks + ' times!';
 }
+$(".clover").innerHTML = cat1;
+$(".aria").innerHTML = cat2;
 
 $( "img" ).click(function(e) {
     const el = e.target;
